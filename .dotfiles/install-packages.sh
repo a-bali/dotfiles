75e93ce708453e589a36bb9d301570e5becf089a
a-bali <a-bali@github.com>
2023-09-07 14:25:42 +0200

    Add tmux

--- .dotfiles/install-packages.sh
+++ .dotfiles/install-packages.sh
@@ -4,9 +4,9 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install --key-bindings --completion --no-update-rc --no-zsh --no-fish
 
 if command -v brew >/dev/null; then
-  brew install bat exa fd tree bash-completion
+  brew install bat exa fd tree bash-completion urlview
 elif command -v apt >/dev/null; then
-  sudo apt install bat exa fd-find tree bash-completion
+  sudo apt install bat exa fd-find tree bash-completion urlview
   mkdir -p ~/.local/bin
   ln -s $(which fdfind) ~/.local/bin/fd
   ln -s $(which batcat) ~/.local/bin/bat
